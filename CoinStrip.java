f4460ef7b8ef423ee30d5c4eef434e5235081ecd
Julia R. Mini <19jrm5@tcl217a-5.cs.williams.edu>
2017-02-09 15:30:30 -0500

    initial commit

---
+++ CoinStrip.java
@@ -0,0 +1,39 @@
+public class CoinStrip {
+    public static void main(String [] args){
+
+
+
+	public CoinStrip(){
+	    int[] gameBoard;
+	    gameBoard = new int[25];
+
+
+	    for(int i = 0; i<25; i0)
+	}
+	
+	
+    }
+
+    
+
+
+    /*
+        Scanner in = new Scanner(System.in);
+        System.out.print( "Give me the coin number you wish to move");
+        int cn = in.nextInt();
+        System.out.print( "Give me a number of spaces");
+        int ns = in.nextInt();
+
+    public CoinStrip(cn){
+	public Random();
+ 
+	Random numbers are the number of spaces between each coin
+	Random rand = new Random(); 
+	/*
+       
+
+      
+
+	    
+    }
+}
